28c7466d50aef580ff82de71487f44882c4a2444
Emmanuel <jaxjoe31@gmail.com>
2023-05-09 13:10:35 +0200

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 onload=()=>{
   moveText("Explore the world’s leading design portfolios")
+  let button=document.getElementById("start-tutorial-button")
+  let link=document.getElementById("click-link")
+  link.onclick=()=>{
   
+   link.setAttribute("href","/tutorial-1.html")
+   let href= link.getAttribute("href")
+ 
+  } 
 }
 
 let moveText=(test)=>{
